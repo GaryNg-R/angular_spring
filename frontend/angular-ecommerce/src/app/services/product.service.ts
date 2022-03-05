8eaecbe26bd0742ea8073bfa8ee67712e1174f6c
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-03-04 22:33:58 -0500

    fix refactor

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -17,6 +17,7 @@ export class ProductService {
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategeoryId: number): Observable<Product[]>{
+
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategeoryId}`;
 
     return this.getProducts(searchUrl);
@@ -28,18 +29,18 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
-  private getProducts(searchUrl: string): Observable<Product[]> {
-    return this.getProducts(searchUrl);
-  }
-  
   getProductCategories(): Observable<ProductCategory[]>{
-
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     );
-  };
+  
+  }
 
-
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
 }
 
 
