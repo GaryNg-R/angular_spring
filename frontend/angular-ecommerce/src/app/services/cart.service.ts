8fbf649700e07e992d888fedd8a574c5fdc3181c
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-04-25 21:53:04 -0400

    add increase and decrease function

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -1,5 +1,5 @@
+import { CartItem } from 'src/app/common/cart-item';
 import { ComponentFixture } from '@angular/core/testing';
-import { CartItem } from './../common/cart-item';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
@@ -79,5 +79,26 @@ export class CartService {
 
   persistCartItems() {
     sessionStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    //get index of item in the array
+    const itemIndex = this.cartItems.findIndex(
+      (tempCartItem) => tempCartItem.id === theCartItem.id
+    );
+    //if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
   }
 }
