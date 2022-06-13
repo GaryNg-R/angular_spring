3aa08659b93e89f088d73cb93088da8a67678a87
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-06-12 22:51:29 -0400

    add validation on credit card and billing

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -91,10 +91,20 @@ export class CheckoutComponent implements OnInit {
         ]),
       }),
       creditCard: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
+        cardType: new FormControl('', [Validators.required]),
+        nameOnCard: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          Luv2ShopValidators.notOnlyWhitespace,
+        ]),
+        cardNumber: new FormControl('', [
+          Validators.required,
+          Validators.pattern('[0-9]{16}'),
+        ]),
+        securityCode: new FormControl('', [
+          Validators.required,
+          Validators.pattern('[0-9]{3}'),
+        ]),
         expirationMonth: [''],
         expirationYear: [''],
       }),
@@ -173,20 +183,33 @@ export class CheckoutComponent implements OnInit {
   get shippingAddressCountry() {
     return this.checkoutFormGroup.get('shippingAddress.country');
   }
-  get buillingAddressStreet() {
-    return this.checkoutFormGroup.get('buillingAddress.street');
+  get billingAddressStreet() {
+    return this.checkoutFormGroup.get('billingAddress.street');
   }
-  get buillingAddressCity() {
-    return this.checkoutFormGroup.get('buillingAddress.city');
+  get billingAddressCity() {
+    return this.checkoutFormGroup.get('billingAddress.city');
   }
-  get buillingAddressState() {
-    return this.checkoutFormGroup.get('buillingAddress.state');
+  get billingAddressState() {
+    return this.checkoutFormGroup.get('billingAddress.state');
   }
-  get buillingAddressZipCode() {
-    return this.checkoutFormGroup.get('buillingAddress.zipCode');
+  get billingAddressZipCode() {
+    return this.checkoutFormGroup.get('billingAddress.zipCode');
   }
-  get buillingAddressCountry() {
-    return this.checkoutFormGroup.get('buillingAddress.country');
+  get billingAddressCountry() {
+    return this.checkoutFormGroup.get('billingAddress.country');
+  }
+
+  get creditCardType() {
+    return this.checkoutFormGroup.get('creditCard.cardType');
+  }
+  get creditCardNameOnCard() {
+    return this.checkoutFormGroup.get('creditCard.nameOnCard');
+  }
+  get creditCardNumber() {
+    return this.checkoutFormGroup.get('creditCard.cardNumber');
+  }
+  get creditCardSecurityCode() {
+    return this.checkoutFormGroup.get('creditCard.securityCode');
   }
 
   copyShippingAddressToBillingAddress(event) {
