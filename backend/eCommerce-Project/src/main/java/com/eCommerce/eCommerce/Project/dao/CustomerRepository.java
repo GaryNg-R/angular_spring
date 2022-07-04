196b25f8d55b1684caa9c1c795c3e9aeae039fad
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-07-03 23:06:22 -0400

    fix duplicate customer in Spring

--- backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/dao/CustomerRepository.java
+++ backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/dao/CustomerRepository.java
@@ -4,4 +4,7 @@ import com.eCommerce.eCommerce.Project.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    //select * from Customer c where c.email = theEmail;  return null if not found
+    Customer findByEmail(String theEmail);
 }
--- backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/service/CheckoutServicelmpl.java
+++ backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/service/CheckoutServicelmpl.java
@@ -39,6 +39,14 @@ public class CheckoutServicelmpl implements CheckoutService{
         order.setShippingAddress(purchase.getShippingAddress());
         //populate customer with order
         Customer customer = purchase.getCustomer();
+
+        //check if this is an existing customer
+        String theEmail = customer.getEmail();
+        Customer customerFromDB = customerRepository.findByEmail(theEmail);
+        if(customerFromDB != null){
+            //We found them ... let's assign them accordingly
+            customer = customerFromDB;
+        }
         customer.add(order);
         //save to the database
         customerRepository.save(customer);
