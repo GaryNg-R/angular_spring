21dc600eae0b2b13afa3c853ea5a0b58137a40ee
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-07-05 14:26:04 -0400

    add allowedOrigins inside webConfig, and remove that from Repository class

--- backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/config/MyDataRestConfig.java
+++ backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/config/MyDataRestConfig.java
@@ -39,6 +39,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         //call an internal helper method
         exposeIds(config);
+
+        //configure cors mappping
+        cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
