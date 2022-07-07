840865745ee9bea744b9fdf7778613a12bdfd7f4
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-07-07 17:08:23 -0400

    add implements HttpInterceptor

--- frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -1,9 +1,22 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthInterceptorService {
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor() {}
 
-  constructor() { }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    throw new Error('Method not implemented.');
+  }
 }
