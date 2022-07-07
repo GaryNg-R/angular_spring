aa3e9394b9dcd698b1ff05685aa35cfc0e270793
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-07-07 16:46:26 -0400

    add 401body

--- backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/config/SecurityConfiguration.java
+++ backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.eCommerce.eCommerce.Project.config;
 
+import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+       //protect endpoint /api/orders
+        http.authorizeHttpRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        //add CORS filters
+        http.cors();
+
+        //force a non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
     }
 }
--- backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/dao/CustomerRepository.java
+++ backend/eCommerce-Project/src/main/java/com/eCommerce/eCommerce/Project/dao/CustomerRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     //select * from Customer c where c.email = theEmail;  return null if not found
+    //@RestResource(path = "find-customer-email")
     Customer findByEmail(String theEmail);
 }
